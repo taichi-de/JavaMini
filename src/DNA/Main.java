cf80b66b119ba1430bd14ccb002bedd8e89257b4
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-05-17 20:29:38 +0200

    base

---
+++ src/DNA/Main.java
@@ -0,0 +1,16 @@
+package DNA;
+
+public class Main {
+  public static void main(String[] args){
+    int[] seq = new int[10];
+
+    for(int i = 0; i < seq.length; i++){
+      seq[i] = new java.util.Random().nextInt(4);
+    }
+
+    for(int i = 0; i < seq.length; i++){
+      char[] base = {'A', 'T', 'G', 'C'};
+      System.out.print(base[seq[i]] + " ");
+    }
+  }
+}
